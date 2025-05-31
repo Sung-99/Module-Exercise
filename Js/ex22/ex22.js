d3a6461cb512465b60f4bd65923990de63a7aff8
Sung <vitorhugomota99@hotmail.com>
2025-05-31 09:30:20 -0300

    up

--- Js/ex22/ex22.js
+++ Js/ex22/ex22.js
@@ -1,7 +1,18 @@
 "use strict";
 function isValidWalk(walk) {
-    if (walk.length > 10 || walk.length%2 != 0)return false;
-    
-    if(walk.length <= 10 ) return true;
+    let n = 0, s = 0, w = 0, e = 0;
+    if (walk.length > 10 || walk.length < 10)
+        return false;
+    for (let x = 0; x < walk.length; x++) {
+        if(walk[x] == 'n' ) n++;
+        if(walk[x] == 's' ) s++;
+        if(walk[x] == 'w' ) w++;
+        if(walk[x] == 'e' ) e++;
+    }
+    if (n === s && w === e)
+        return true;
+    return false;
 }
+
+
 module.exports = isValidWalk;
--- Js/ex22/ex22.ts
+++ Js/ex22/ex22.ts
@@ -1,8 +1,16 @@
 function isValidWalk(walk:any) {
+ 
+  let n=0, s=0, w=0, e=0;
+  if (walk.length > 10 || walk.length%2 != 0) return false;
+   for (let x = 0; x < walk.length; x++) {
+        if(walk[x] == 'n' ) n++;
+        if(walk[x] == 's' ) s++;
+        if(walk[x] == 'w' ) w++;
+        if(walk[x] == 'e' ) e++;
+    }
 
-  if (walk.length > 10 || walk.length/2 != 0) return false
+   return (n === s && w === e);
   
-  if(walk.length <= 10 )return true;
 }
 
 module.exports = isValidWalk;
