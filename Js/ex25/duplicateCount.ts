3d3e9967054117e30dcacc65a3e441429a52394b
Sung <vitorhugomota99@hotmail.com>
2025-06-04 23:05:46 -0300

    up

---
+++ Js/ex25/duplicateCount.ts
@@ -0,0 +1,15 @@
+export const duplicateCount = (phrase: string): number => {
+    let setA = new Set<string>();
+     let setB = new Set<string>();
+    let cont = 0;
+      for (const char of phrase.toLowerCase()){
+        if ((char >= 'a' && char <= 'z') && (char >= '0' && char <= '9')) {
+            if (setA.has(char)) {
+                setB.add(char);
+            }
+
+            setA.add(char);
+        }
+    }
+    return setB.size;
+}
